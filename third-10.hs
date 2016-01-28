4bbdaba7d614bc8d00f8e50c1cb4d14de52d2d92
luke <nerorevenge@gmail.com>
2016-01-28 16:53:07 -0500

    I have a solution I am proud off

--- third-10.hs
+++ third-10.hs
@@ -1,4 +1,5 @@
- -- Problem 21 
+import System.Random 
+import Data.List
 
 -- Problem 21 
 insertAt :: Eq a => a -> [a] -> Int -> [a]
@@ -16,4 +17,25 @@ range a b = let recFunction x
                    | otherwise = []
             in recFunction a
 
--- to be continued after I learn how to deal with random number generators in haskell
+-- Problem 23
+rnd_select :: [a] -> Int -> IO [a]
+rnd_select list number = do
+		    gen <- newStdGen
+                    let newList = take number $ randomRs (0,(length list)-1) gen
+                    return [list !! n | n <- newList]
+                    
+
+-- Problem 25  I can't believe I got this to work .. sniff I would like to thank applicative functors and my mom 
+rnd_permu :: (Eq a) => [a] -> IO [a]
+rnd_permu [] = return []
+rnd_permu [x] = return  [x]
+rnd_permu l@(x:xs) = do
+                     gen <- newStdGen
+                     let (randomIndex,_) = randomR (0,(length l)-1) gen                 
+                         randomElement = l !! randomIndex
+                         restOfTheList = l \\ [randomElement]
+                     (randomElement:) <$> (rnd_permu restOfTheList)
+
+
+
+
